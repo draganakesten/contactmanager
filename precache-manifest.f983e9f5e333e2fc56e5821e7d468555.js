8b5e0dc411532055de827ba5bedd1174fa307784
Dragana Kesten <draganakesten3@gmail.com>
2019-01-10 14:45:37 +0100

    Updates

---
+++ precache-manifest.f983e9f5e333e2fc56e5821e7d468555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "0e84d2f9a54a2014bbe9",
+    "url": "/contactmanager/static/js/main.0e84d2f9.chunk.js"
+  },
+  {
+    "revision": "e0d92c424b17e12b4c54",
+    "url": "/contactmanager/static/js/1.e0d92c42.chunk.js"
+  },
+  {
+    "revision": "0e84d2f9a54a2014bbe9",
+    "url": "/contactmanager/static/css/main.4ab803a3.chunk.css"
+  },
+  {
+    "revision": "e0d92c424b17e12b4c54",
+    "url": "/contactmanager/static/css/1.388b5613.chunk.css"
+  },
+  {
+    "revision": "67fbb2cb5f8c2a2b17e122e0886973c6",
+    "url": "/contactmanager/index.html"
+  }
+];
